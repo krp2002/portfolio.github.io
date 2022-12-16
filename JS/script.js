1e8f0da2d1a1db8ee28d7e3e7048b690d3bcd53a
krp2002 <95630287+krp2002@users.noreply.github.com>
2022-12-16 14:10:08 +0530

    Personal Portfolio Web

---
+++ JS/script.js
@@ -0,0 +1,104 @@
+// typint animation
+
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Coder"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true,
+})
+
+// aside
+
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length;
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    
+    for(let i=0; i<totalNavList; i++){
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click", function(){
+            
+            removeBackSection();
+            for(let j=0; j<totalNavList; j++){
+                if(navList[j].querySelector("a").classList.contains("active")){
+                    addBackSection(j);
+                    // allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+            if(window.innerWidth < 1200){
+                asideSectionTogglerBtn();
+            }
+        })
+    }
+
+    function removeBackSection(){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("back-section");
+        }
+    }
+
+    function addBackSection(num){
+        allSection[num].classList.add("back-section");
+    }
+    function showSection(element){
+
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
+
+    function updateNav(element){
+        for(let i=0; i<totalNavList; i++){
+            navList[i].querySelector("a").classList.remove("active");
+            const target = element.getAttribute("href").split("#")[1];
+
+            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+                navList[i].querySelector("a").classList.add("active");
+            }
+        }
+    }
+    document.querySelector(".hire-me").addEventListener("click", function() {
+        const sectionIndex = this.getAttribute("data-section-index");
+        // console.log(sectionIndex);
+        showSection(this);
+        updateNav(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+    })
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+        aside = document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () => {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn(){
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.toggle("open");
+            }
+        }
+
+    function sendEmail(){
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "krunalpatel6802@gmail.com",
+            Password : "FE611C47590A5750B5C8575CE229642422CF",
+            To : 'krunalpatel6802@gmail.com',
+            From : document.getElementById("email").value,
+            Subject : "New contact form enquiry",
+            // Body : "new",
+            Body : "Name : " + document.getElementById("name").value
+            + "<br> Email : " + document.getElementById("email").value
+            + "<br> Subject : " + document.getElementById("sub").value
+            + "<br> Message : " + document.getElementById("msg").value,
+        }).then(
+          message => alert(message)
+        );
+    }
